9fc0e583d39228e61ade517661fcf3910a8b223e
Mohammad-Shameem <mehmetsaki789@gmail.com>
2022-04-26 12:35:12 +0600

    genius car service with server mongodb

---
+++ index.js
@@ -0,0 +1,65 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+const cors = require("cors");
+const res = require('express/lib/response');
+
+
+
+//use MiddleWare
+app.use(cors());
+app.use(express.json());
+app.get
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sqkfu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+        await client.connect()
+        const serviceCollection = client.db("Genius-Car-Service").collection("Service");
+        app.get('/service', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        });
+        app.post("/service", async (req, res) => {
+            const newService = req.body
+            console.log(newService)
+            const result = await serviceCollection.insertOne(newService)
+            res.send(result)
+        });
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await serviceCollection.deleteOne(query)
+            res.send(result)
+        })
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+
+
+app.get('/', (req, res) => {
+    res.send("Genius Car Service Server is Running")
+})
+
+app.listen(port, () => {
+    console.log("Listening to port", port)
+
+})
